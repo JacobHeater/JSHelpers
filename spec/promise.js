dcb9cefa550bb7bd58d5937b20d2fbbd3760346d
Jacob Heater <jacobheater@Jacobs-MacBook-Pro.local>
2017-02-01 22:35:46 -0500

    Revert sequence back to sync calls. Add  and tests.

---
+++ spec/promise.js
@@ -0,0 +1,59 @@
+(() => {
+
+    'use strict';
+
+    const $Promise = require('../promise.js');
+    const vars = require('./vars.js');
+    const FAIL_MSG = 'Whoops!';
+
+    let output = '';
+    let failOutput = '';
+
+    describe('Parallel Task Running Tests', () => {
+
+        beforeEach(go => {
+            output = ''; //reset to default.
+
+            let promise = new $Promise();
+
+            setTimeout(function() {
+                promise.resolve(vars.HELLO);
+            }, 100);
+
+            promise.then(function(val) {
+                output += val;
+                let promise = new $Promise();
+
+                setTimeout(function() {
+                    promise.resolve(vars.WORLD);
+                }, 0);
+
+                promise.then(function(val) {
+                    output += val;
+                });
+            });
+
+            let errPromise = new $Promise();
+
+            setTimeout(function() {
+                errPromise.reject(FAIL_MSG);
+            }, 400);
+
+            errPromise.then(function() {
+                output += FAIL_MSG;
+            });
+
+            errPromise.fail(function(val) {
+                failOutput += val;
+                go();
+            });
+        });
+
+        it('Should resolve the promise and the output should equal vars.HELLO_WORLD', () => {
+            expect(output).toEqual(vars.HELLO_WORLD);
+            expect(failOutput).toEqual(FAIL_MSG);
+        });
+
+    });
+
+})();
